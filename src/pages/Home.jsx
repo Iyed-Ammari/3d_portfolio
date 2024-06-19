02d454feb5480c68b1b26bfcc4ba5f4cf53ba168
Iyed Ammari <ammariiyed@isamm.u-manouba.tn>
2024-06-19 01:29:49 +0100

    chore: Refactor Home component and add 3D models
    Refactor the Home component to improve code readability and organization. Add 3D models for the sky, bird, and plane using the `@react-three/fiber` library. Adjust the island model for different screen sizes.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,10 @@
 import { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
-import Loader from "../components/Loader";  
-import  Island  from "../models/Island";
+import Loader from "../components/Loader";
+import Island from "../models/Island";
 import Sky from "../models/Sky";
+import Bird from "../models/Bird";
+import Plane from "../models/Plane";
 
 const Home = () => {
   const adjustIslandForScreenSize = () => {
@@ -16,9 +18,10 @@ const Home = () => {
       screenScale = [1, 1, 1];
     }
     return [screenScale, screenPosition, rotation];
-  }
+  };
 
-  const [islandScale, islandPosition, islandRotation] = adjustIslandForScreenSize();
+  const [islandScale, islandPosition, islandRotation] =
+    adjustIslandForScreenSize();
   return (
     <section className="w-full h-screen relative">
       {/* <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center  ">
@@ -29,19 +32,26 @@ const Home = () => {
         camera={{ near: 0.1, far: 1000 }}
       >
         <Suspense fallback={<Loader />}>
-          <directionalLight position={[1,1,1]} intensity={2} />
+          <directionalLight position={[1, 1, 1]} intensity={2} />
           <ambientLight intensity={0.5} />
           {/* <pointLight /> 
            I removed the pointlight because we're outside and we have the sun (the directional light and the ambient light) */}
           {/* <spotLight />
           spotLight is similar to pointlight but in the shape of a cone so I won't be needing it as well */}
-          <hemisphereLight skyColor="#b1e1ff" groundColor="#000000" intensity={1} />
-          
-          <Sky/>
+          <hemisphereLight
+            skyColor="#b1e1ff"
+            groundColor="#000000"
+            intensity={1}
+          />
+
+          <Bird />
+          <Sky />
           <Island
             position={islandPosition}
             scale={islandScale}
-            rotation={ islandRotation} />
+            rotation={islandRotation}
+          />
+          <Plane />
         </Suspense>
       </Canvas>
     </section>
