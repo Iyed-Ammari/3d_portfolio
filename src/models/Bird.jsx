ffa3cf10d432beaffd147b9488dd136b19377f36
Iyed Ammari <ammariiyed@isamm.u-manouba.tn>
2024-07-11 03:28:18 +0100

    feat: Implement lazy loading for 3D bird model in Bird component

--- src/models/Bird.jsx
+++ src/models/Bird.jsx
@@ -2,43 +2,66 @@ import React, { useEffect, useRef } from "react";
 import birdScene from "../assets/3d/bird.glb";
 import { useAnimations, useGLTF } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
-
+import * as THREE from "three";
+import { gsap } from "gsap";
 const Bird = () => {
+//   const axesHelper = new THREE.AxesHelper(10); // 5 is the length of the axes
+// axesHelper.scale.set(10, 10, 10);
   const birdRef = useRef();
 
   const { scene, animations } = useGLTF(birdScene);
-
+// scene.add(axesHelper);
   const { actions } = useAnimations(animations, birdRef);
   useEffect(() => {
     actions["Take 001"].play();
   }, []);
-   useFrame(({ clock, camera }) => {
-     // Update the Y position to simulate bird-like motion using a sine wave
-     birdRef.current.position.y = Math.sin(clock.elapsedTime) * 0.2 + 2;
+useFrame(({ clock, camera }) => {
+  // Update the Y position to simulate bird-like motion using a sine wave
+  birdRef.current.position.y = Math.sin(clock.elapsedTime) * 0.2 + 2;
 
-     // Check if the bird reached a certain endpoint relative to the camera
-     if (birdRef.current.position.x > camera.position.x + 10) {
-       // Change direction to backward and rotate the bird 180 degrees on the y-axis
-       birdRef.current.rotation.y = Math.PI;
-     } else if (birdRef.current.position.x < camera.position.x - 10) {
-       // Change direction to forward and reset the bird's rotation
-       birdRef.current.rotation.y = 0;
-     }
+  // Check if the bird reached a certain endpoint relative to the camera
+  
 
-     // Update the X and Z positions based on the direction
-     if (birdRef.current.rotation.y === 0) {
-       // Moving forward
-       birdRef.current.position.x += 0.01;
-       birdRef.current.position.z -= 0.01;
-     } else {
-       // Moving backward
-       birdRef.current.position.x -= 0.01;
-       birdRef.current.position.z += 0.01;
-     }
-    
-   });
+
+    // Check if the bird reached a certain endpoint relative to the camera
+    if (birdRef.current.position.x > camera.position.x + 10) {
+      // Change direction to backward and rotate the bird 180 degrees on the y-axis
+      gsap.fromTo(
+        birdRef.current.rotation,
+        {
+          y: birdRef.current.rotation.y,
+        },
+        {
+          y: Math.PI,
+          duration: 0.5,
+        }
+      );
+  }
+  if (birdRef.current.position.x < camera.position.x - 10) {
+    console.log('eeeee')
+      // Change direction to forward and reset the bird's rotation
+    birdRef.current.rotation.y = 0;
+    }
+
+    // Update the X and Z positions based on the direction
+  if (birdRef.current.rotation.y === 0) {
+    // Moving forward
+    birdRef.current.position.x += 0.04;
+    birdRef.current.position.z -= 0.04;
+  } else {
+    // Moving backward
+    birdRef.current.position.x -= 0.04;
+    birdRef.current.position.z += 0.04;
+  }
+  });
+
   return (
-    <mesh ref={birdRef} position={[-5, 2, 1]} scale={[0.003, 0.003, 0.003]}>
+    <mesh
+      ref={birdRef}
+      position={[-5, 2, 1]}
+      scale={[0.003, 0.003, 0.003]}
+      
+    >
       <primitive object={scene} />
     </mesh>
   );
