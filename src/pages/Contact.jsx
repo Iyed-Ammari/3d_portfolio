fc41afedb5946dcfd9ddf0438ad612fcde9e407b
Iyed Ammari <ammariiyed@isamm.u-manouba.tn>
2024-07-13 01:11:54 +0100

    feat: Update contact form placeholders and recipient names
    Adjust the placeholders in the contact form to display "Iyed Ammari" for the name field and "Iyed@gmail.com" for the email field. Update the recipient names in the email template to "Iyed Ammari" and "iyediyedammari@gmail.com" respectively. This ensures that the form fields provide clear instructions to the users and the emails are sent to the correct recipients.

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -32,9 +32,9 @@ const Contact = () => {
         import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
         {
           from_name: form.name,
-          to_name: "JavaScript Mastery",
+          to_name: "Iyed Ammari",
           from_email: form.email,
-          to_email: "sujata@jsmastery.pro",
+          to_email: "iyediyedammari@gmail.com",
           message: form.message,
         },
         import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
@@ -90,7 +90,7 @@ const Contact = () => {
               type="text"
               name="name"
               className="input"
-              placeholder="John"
+              placeholder="Iyed Ammari"
               required
               value={form.name}
               onChange={handleChange}
@@ -104,7 +104,7 @@ const Contact = () => {
               type="email"
               name="email"
               className="input"
-              placeholder="John@gmail.com"
+              placeholder="Iyed@gmail.com"
               required
               value={form.email}
               onChange={handleChange}
